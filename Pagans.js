964e2cc7829a0418224f81ecec9c057de3236914
Jean Marie Kévin <kjeanmarie01@gmail.com>
2023-02-21 15:49:47 +0100

    update function done

--- Pagans.js
+++ Pagans.js
@@ -8,17 +8,17 @@ const conf = require('./config');
 const genereTxtAlea = (nBchar) => {
 
     let txt = ""
-    //     // faire une boucle de nb de char 
+    //     // faire une boucle de nb de char
     for (let i = 0; i < nBchar; i++) {
         // console.log(nBchar)
         let charNom = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        // (extraire de façon aléatoire un caractères dans une chaine de caractère autorisé) 
+        // (extraire de façon aléatoire un caractères dans une chaine de caractère autorisé)
         let debutrandomstr = Math.floor(Math.random() * charNom.length);
 
         // utilisation de subtring pour extraire la chaine entre debutrandom et endrandom
         txt += charNom.substring(debutrandomstr, debutrandomstr + 1);
     }
-    //     //fonction delete dans pagans.init avant de générer les nouveau nom 
+    //     //fonction delete dans pagans.init avant de générer les nouveau nom
     //console.log(txt);
     return txt
     //     // me sortir une chaine de texte avec un certains nombre de caractères correspondant à la variable nbchar
@@ -52,11 +52,11 @@ Pagans.search = (searchstring) => {
     //console.log(`liste des Pagans contenant ${searchstring} dans le nom`)
     glob.sync(`${conf.maindir}/data/pagans/*.json`)
         .forEach(f => {
+            //console.log(f)
             const pagan = fs.readJsonSync(f);
-            //console.log(pagan.nom)
-            if (pagan.nom.includes(searchstring)) {
+            if (pagan.nom && pagan.nom.includes(searchstring)) {
 
-                //console.log(pagan)
+                console.log(pagan)
             }
         })
 }
@@ -65,63 +65,69 @@ Pagans.search = (searchstring) => {
 Pagans.update = (UUID, data) => {
     // vérifier si UUID existe
     // si il existe on l'ecrase avec les nouvelles data
-    glob.sync(`${conf.maindir}/data/pagans/*.json`)
-        .forEach(f => {
-            console.log(f)
-            fs.writeJsonSync(f, data)
-            if (!fs.pathExistsSync(`${conf.maindir}/data/pagans/${UUID}.json`)) {
-                console.log(`n'existe pas ${conf.maindir}/data/pagans/${UUID}.json`)
-            } else {
-                fs.writeJsonSync(f, data)
-            }
+    if (fs.existsSync(`${conf.maindir}/data/pagans/${UUID}.json`)) {
+        fs.writeJsonSync(`${conf.maindir}/data/pagans/${UUID}.json`, data)
+        console.log(`Le pagan ${UUID} à été update`)
+    } else {
+        console.log(`Le pagan ${UUID} n'existe pas`)
+    }
+}
 
-            const postPagans = fs.readJsonSync(f, data)
-            console.log(postPagans)
+Pagans.get = (UUID) => {
+    // l'instruction que j'éxécute
+    try {
+        // récupérer les info du fichier et les lire
+        const info = fs.readJsonSync(`${conf.maindir}/data/pagans/${UUID}.json`)
+        console.log(info)
 
-            //const PagData = Pagans.PagData(UUID, data)
-            //let newData = "it's a new data";
-            //glob.sync me génère le tableau
-            //const Pag = fs.writeJsonSync('C:/Users/keke-/OneDrive/Documents/informatique/stage/testkevin/data/pagans/*.json', UUID, data, { data: newData })(f)
-            // if (Pag.status === (err)) {
-            //     return console.error(err)
-            // } else {
-            //     return console.log(newData)
-            // }
+        //parser le fichier pour transformer mon JSON en objet
+        const infoData = JSON.parse(info)
+        return infoData
 
-        })
-    //console.log(Pagans.updatePag)
-
+    } catch (error) {
+        console.log("erreur")
+    }
 }
 
 
+//PagansUUID.search = (UUID) => {
+//console.log(`liste des Pagans contenant un UUID`)
+// glob.sync(`${conf.maindir}/data/pagans/${UUID}.json`)
+//.forEach(f => {
+// console.log(f)
+//const pagan = fs.readJsonSync(f);
+//if (pagan.nom && pagan.nom.includes(searchstring)) {
 
-//TextG.init = (randomText) => {
-
-
-// faire ma boucle
-// for (let i = 10; i <= randomText; i++) {
-//     const UniqueID = (UUID.v4())
-
-//générer ma chaine de caractere entre 10 et 255
-// generaterandomtext(length) = () => {
-//     let all = "";
-//     const charactersName = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
+//  console.log(pagan)
 // }
-// const randomLg = randomText(10, 255);
-// const randomStr = generaterandomtext(randomLg)
-
-
-
-//console.log(randomStr);
-
-// }
+//   })
 //}
 
+//récupérer les infos et les lire // utiliser get dans main.js //
+// faire la fonction search pour récuperer les UUID et les afficher avec la fonction get
 
 
-// utiliser glob.sync et read-json sync
 
+
+//console.log(UUID)
+//console.log(glob.sync(`${conf.maindir}/data/pagans/*.json`))
+// glob.sync(`${conf.maindir}/data/pagans/*.json`)
+//     .forEach(f => {
+//         console.log(f)
+//         fs.writeJsonSync(f, data)
+//         if (!fs.pathExistsSync(`${conf.maindir}/data/pagans/${UUID}.json`)) {
+//             console.log(`n'existe pas ${conf.maindir}/data/pagans/${UUID}.json`)
+//         } else {
+//             fs.writeJsonSync(f, data)
+//         }
+
+//         const postPagans = fs.readJsonSync(f, data)
+//         console.log(postPagans)
+
+//     })
+//console.log(Pagans.updatePag)
+
+//}
 
 
 
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const fs = require('fs-extra');
 const Pagans = require("./Pagans");
 
-// genererfile.init(100);
-console.log(Pagans)
-// Pagans.init(255)
-Pagans.search("AB")
-Pagans.update("0a1673bf-28b5-47b5-9387-8bea44021ffc")
 
-console.log("ok");
+console.log(Pagans)
+//Pagans.init(255)
+//Pagans.search("AB")
+//Pagans.update("fdf6d519-625b-443f-890b-946dc13ffb04", { UUID: "fdf6d519-625b-443f-890b-946dc13ffb04", nom: "toto" })
+Pagans.get("fdf6d519-625b-443f-890b-946dc13ffb04")
+//PagansUUID.search()
+
+//console.log("ok");
 
 
